4636332cd3d206d06240641e754610c88f7f6123
Joseph-kirigo <josephkirigo777@gmail.com>
2022-08-20 06:47:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+// let companyName ='Scuber';
+const companyName = 'Scuber';
+mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
+
